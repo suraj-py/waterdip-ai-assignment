b22030adf968cf4da5de1fb6b28b45a7b31eb100
Suraj Mhatre <surajmcodes@gmail.com>
2023-11-22 01:39:34 +0530

    test: added simple testcase to check the whether the created task is correct or not

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        # Create a tasks
+        test_todo = Todo.objects.create(
+            title='My First Task',
+            is_completed=False)
+        test_todo.save()
+
+    def test_todo_content(self):
+        todo = Todo.objects.get(id=1)
+        title = f'{todo.title}'
+        is_completed = f'{todo.is_completed}'
+        self.assertEqual(title, 'My First Task')
+        self.assertTrue(is_completed, False)
